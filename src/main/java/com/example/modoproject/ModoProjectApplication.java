4dc73a9517c2c0d3aba768c9a81a82fbfdb9cd3b
Eunbin Seo <sso061356@naer.com>
2024-03-21 20:48:11 +0900

    환경구성

---
+++ src/main/java/com/example/modoproject/ModoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.modoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/modoproject/ModoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
