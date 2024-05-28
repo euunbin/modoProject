f26862f249d943a67b1b1b4c17d5d74a8bffd62f
Eunbin Seo <sso061356@naver.com>
2024-05-29 00:07:43 +0900

    결제_(사용자의 닉네임 받아오기)

--- src/main/java/com/example/modoproject/controller/loginController.java
+++ src/main/java/com/example/modoproject/controller/loginController.java
@@ -3,10 +3,12 @@ package com.example.modoproject.controller;
 import com.example.modoproject.entity.User; // 올바른 User 클래스 import
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Optional;
 import java.util.logging.Logger;
@@ -50,4 +52,18 @@ public class loginController {
             return "login";
         }
     }
-}
+
+    @Autowired
+    private HttpSession httpSession;
+
+    @GetMapping("/user/nickname")
+    @ResponseBody
+    public String getUserNickname() {
+        User user = (User) httpSession.getAttribute("user");
+        if (user != null) {
+            return user.getNickname();
+        } else {
+            return ""; // 또는 사용자가 로그인하지 않은 경우 처리
+        }
+    }
+    }
